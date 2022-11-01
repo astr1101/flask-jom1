ddc997a2dee9e85c8800e167cb22daa3366cdbf4
astr1101 <31046851+astr1101@users.noreply.github.com>
2022-11-01 16:55:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/capitals')
+def get_capitals():
+    import mysql.connector
+    list_of_countries=[]
+
+    cnx = mysql.connector.connect(user='root', database='railway', host='containers-us-west-30.railway.app', password='uRmUBE9EPeW2Q73NQWgm', port='6811')
+    cursor = cnx.cursor()
+
+    import random
+    query = ("SELECT * FROM country")
+    cursor.execute(query) 
+    for (test) in cursor:
+      list_of_countries.append(test[2])
+    return(jsonify(sorted(list_of_countries)))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
